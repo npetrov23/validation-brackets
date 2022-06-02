a1160bf82b184392996eb8e8b70fc64bc0185439
npetrov23 <npetrov2309@gmail.com>
2022-06-02 23:44:10 +0500

    Правки 2

--- vendor/brackets.php
+++ vendor/brackets.php
@@ -7,23 +7,30 @@ function brackets($string) {
         $stack = [];
         $symbols = ["(", ")", "[", "]", "{", "}", "<", ">"];
 
-        for($position_letter = 0; $position_letter < $count_letters; $position_letter++){
-            if(in_array($string[$position_letter], $symbols)) {
-                $sym = $string[$position_letter];
-                $key_sym = array_search($sym, $symbols);
-                if($key_sym % 2 == 0) {
-                    array_push($stack, $sym);
-                }
-                else {
-                    if(empty($stack) || $symbols[$key_sym-1] != end($stack)) {
-                        $validation = false;
-                        break;
+        if (!preg_match('#[\(\)\[\]\{\}]#', $string)) {
+            $validation = false;
+        }
+        else
+        {
+            for($position_letter = 0; $position_letter < $count_letters; $position_letter++){
+                if(in_array($string[$position_letter], $symbols)) {
+                    $sym = $string[$position_letter];
+                    $key_sym = array_search($sym, $symbols);
+                    if($key_sym % 2 == 0) {
+                        array_push($stack, $sym);
                     }
-                    array_pop($stack);
+                    else {
+                        if(empty($stack) || $symbols[$key_sym-1] != end($stack)) {
+                            $validation = false;
+                            break;
+                        }
+                        array_pop($stack);
+                    }
+                    
                 }
-                
             }
         }
+
 
         if($validation && !empty($stack)) {
             $validation = false;
